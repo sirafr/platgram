74c9f214e8cc70122b5c7b8927d1b7ce04a44d93
sirafr <andres22f@hotmail.com>
2021-07-18 23:50:00 -0500

    clase17 reto json

---
+++ platzigram/views.py
@@ -0,0 +1,34 @@
+"""PLATZIGRAM VIEWS"""
+
+# Django
+from django.http import HttpResponse
+
+# Utilities
+from datetime import datetime
+import json
+
+
+def hello_world(request):
+	now = datetime.now().strftime('%b %dth, %Y - %H:%M hrs.')
+	return HttpResponse(f"HOLA MUNDO! server time: {now}")
+
+
+def hi(request):
+	"""hi"""
+	numbers = [int(i) for i in request.GET['numbers'].split(',')]
+	sorted_ints = sorted(numbers)
+	# import pdb; pdb.set_trace()
+
+	# Tranformacion a API
+	data = {
+		'status':'ok',
+		'numbers':sorted_ints,
+		'message':'Integers sorted sucessfully!'
+	}
+
+	# Regresamos la data en formato JSON
+	return HttpResponse(json.dumps(data),content_type='application/json')
+
+
+
+
