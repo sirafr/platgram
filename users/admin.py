ec0c951d737becc43d24b0d2424ff09c5039cc17
sirafr <andres22f@hotmail.com>
2021-08-09 17:39:30 -0500

    class 17 dashboard admin

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,10 @@
 """USER ADMIN"""
 # Django
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 # Models
+from django.contrib.auth.models import User
 from users.models import Profile
 
 # Registro de modelos
@@ -24,3 +26,48 @@ class ProfileAdmin(admin.ModelAdmin):
     'user__is_active',
     'user__is_staff',
     )
+
+    fieldsets = (
+        ('Profile',{
+            'fields':(
+                ('user','picture'),
+            ),
+        }),
+        ('Extra info',{
+            'fields':(
+                ('website','phone_number'),
+                ('biography')
+            )
+        }),
+        ('Metadata',{
+            'fields':(
+                ('created','modified'),),
+        })
+    )
+
+    readonly_fields = ('created','modified','user')
+
+
+class ProfileInline(admin.StackedInline):
+    """Profile inline admin for users"""
+
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+
+class UserAdmin(BaseUserAdmin):
+    """Add profile admin to base user admin."""
+
+    inlines = (ProfileInline,)
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff',
+    )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
