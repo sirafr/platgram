831e269c7f2a3f36ee9fb4fbf7385acca392bab8
sirafr <andres22f@hotmail.com>
2021-08-09 10:57:46 -0500

    class 15

---
+++ users/apps.py
@@ -0,0 +1,11 @@
+"""User App configuration"""
+
+from django.apps import AppConfig
+
+
+class UsersConfig(AppConfig):
+    """User App Config"""
+
+    
+    name = 'users'
+    verbose_name = 'Users'
