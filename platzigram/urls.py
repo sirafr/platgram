d625e7ee249d79b2a4f0a7be248934024053bca2
sirafr <andres22f@hotmail.com>
2021-08-22 11:58:53 -0500

    logout class 21

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -35,6 +35,7 @@ urlpatterns = [
     path('sorted/',local_views.sorted_integers,name='sort'),
     path('hi/<str:name>/<int:age>/',local_views.say_hi,name='hi'),
     path('posts/', posts_views.list_post,name='feed'),
-    path('users/login',user_views.login_view,name='login')
+    path('users/login',user_views.login_view,name='login'),
+    path('users/logout',user_views.logout_view,name='logout')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 """USER VIEWS"""
 
 # Django
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 
 def login_view(request):
@@ -22,3 +23,9 @@ def login_view(request):
 
     
     return render(request,'users/login.html')
+
+@login_required
+def logout_view(request):
+    """Logout view"""
+    logout(request)
+    return redirect('login')
